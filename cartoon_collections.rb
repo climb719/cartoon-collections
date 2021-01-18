1c7f60e4cb2fca91f19bc95a4f9a44e4253fc097
climb719 <mary.oldweiler@gmail.com>
2021-01-18 14:03:02 -0500

    commit tree 447bd2fa2a5a27b5e2bc05754942570865477875

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarfs)
+ dwarfs.each_with_index do |name, index| 
+  puts "#{index+1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  elements.map { |element| element.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  calls_long.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ingredients.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
+
+
